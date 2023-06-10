51b838f5c6d796a0cf99a0eb61f5742b550cbf32
Juan Rodríguez <juanpapiro@hotmail.com>
2023-06-10 20:33:38 -0300

    path

--- src/main/java/br/com/texthelper/App.java
+++ src/main/java/br/com/texthelper/App.java
@@ -16,12 +16,16 @@ public class App {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
-		Reflections reflections = new Reflections(
-				"br.com.texthelper",
-				new SubTypesScanner(false),
-				ClasspathHelper.forClassLoader());
-		Set<Class<?>> listClass = reflections.getSubTypesOf(Object.class);
-		listClass.forEach(c -> TextHelperLog.info("class -> " + c.getName()));
+		try {
+			Reflections reflections = new Reflections(
+					"br.com.texthelper.parsers",
+					new SubTypesScanner(false),
+					ClasspathHelper.forClassLoader());
+			Set<Class<?>> listClass = reflections.getSubTypesOf(Object.class);
+			listClass.forEach(c -> TextHelperLog.info("class -> " + c.getName()));			
+		} catch(Exception e) {
+			TextHelperLog.error("Falha ao listar classes", e);
+		}
 	}
 
 }

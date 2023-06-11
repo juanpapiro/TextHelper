b3e2eeec8ee34b03ba76ed3b1b6029048e0e7f33
Juan Rodríguez <juanpapiro@hotmail.com>
2023-06-10 23:48:28 -0300

    path

--- src/main/java/br/com/texthelper/controller/TextHelperController.java
+++ src/main/java/br/com/texthelper/controller/TextHelperController.java
@@ -1,16 +1,32 @@
 package br.com.texthelper.controller;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.texthelper.App;
 import br.com.texthelper.models.RequestSimple;
+import br.com.texthelper.parsers.TypeParser;
 import br.com.texthelper.service.ToObject;
 import br.com.texthelper.service.impl.ToObjectImpl;
+import br.com.texthelper.utils.TextHelperLog;
 
 @RestController
 public class TextHelperController {
 
+	@Autowired
+	private ResourceLoader resourceLoader;
 	
 	@GetMapping("/parse")
 	public Object parse(@RequestParam String text) {
@@ -22,5 +38,21 @@ public class TextHelperController {
 		}
 	}
 	
+	@GetMapping("/listclass")
+	public Object find() {
+		try {
+			String pathResurce = App.class.getPackageName().replaceAll("[.]", "/");
+			TextHelperLog.info("Iniciando listagem de classes: " + App.class.getPackageName());
+			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+			InputStream is = classLoader.getResourceAsStream(pathResurce);
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			List<String> locateClasses = br.lines().collect(Collectors.toList());
+			locateClasses.forEach(locateClass -> TextHelperLog.info("Classe localizada: " + locateClass));
+			return locateClasses;			
+		} catch(Exception e) {
+			TextHelperLog.error("Falha ao ler stream de bytes de resource.", e);
+			return new ArrayList<>();
+		}
+	}
 
 }
